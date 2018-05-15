98fcc06ec167dc30abec59aec8ed45467c7f9176
Johannes Postler <johannes.postler@uibk.ac.at>
2018-05-15 14:09:04 +0200

    Autodelete unused images in STM

--- stm/apps.py
+++ stm/apps.py
@@ -7,3 +7,6 @@ from django.apps import AppConfig
 class StmConfig(AppConfig):
     name = 'stm'
     verbose_name = 'Awesome STM'
+
+    def ready(self):
+        import stm.signals
---
+++ stm/signals.py
@@ -0,0 +1,9 @@
+from django.db.models.signals import post_delete
+from django.dispatch.dispatcher import receiver
+
+from stm.models import Image
+
+@receiver(post_delete, sender=Image)
+def image_delete(sender, instance, **kwargs):
+    # Pass false so FileField doesn't save the model.
+    instance.preview_image.delete(False)
