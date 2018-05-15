7d7fd2e041cfe0d5f8faeca54cc138b8fa505029
Johannes Postler <johannes.postler@uibk.ac.at>
2018-05-15 15:45:01 +0200

    Further STM usability: rating and image preview

--- stm/admin.py
+++ stm/admin.py
@@ -4,11 +4,29 @@ from __future__ import unicode_literals
 from django.contrib import admin
 from stm.models import Sample, Measurement, Image, Operator, StandardOperatingProcedure
 from stm.tasks import read_images_async
+from django.conf import settings
 
 class ImageAdmin(admin.ModelAdmin):
-    list_display = ('name', 'type', 'measurement')
-    list_filter = ('type',)
+    def stars(self, obj):
+        if obj.image_rating:
+            # UTF8 to the rescue!
+            return '<span>{}{}</span>'.format(obj.image_rating*'★', (5-obj.image_rating)*'☆')
+        else:
+            return ''    
+    stars.allow_tags = True
+
+    def image_display(self, obj):
+        try:
+            return '<img src=\'{}\' style="max-height: 60px" />'.format(obj.preview_image.url)
+        except ValueError:
+            return ''
+    image_display.allow_tags = True
+
+    list_display = ('name', 'type', 'measurement', 'image_display', 'stars')
+    list_filter = ('type', 'image_rating', )
     search_fields = ('measurement__name', 'measurement__sample__material', 'name')
+    list_display_links = ('name', 'image_display')
+    save_on_top = True
 
 class MeasurementAdmin(admin.ModelAdmin):
     list_display = ('id', 'sample', 'name', 'operator', 'time', 'tip_type')
