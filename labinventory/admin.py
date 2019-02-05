fbcc3f60fd69f3a7f7ce1527138629c52f9ba2e4
Felix <felix.duensing@uibk.ac.at>
2019-02-05 15:36:49 +0100

    LABINVENTORY: add application for lab inventory. one model for pressure gauges

---
+++ labinventory/admin.py
@@ -0,0 +1,57 @@
+import os
+
+from django.contrib import admin
+from django.http import HttpResponse
+from openpyxl import load_workbook
+from openpyxl.writer.excel import save_virtual_workbook
+
+from labinventory.models import PressureGaugeUsageRecord, PressureGauge, GaugeType
+
+
+class PressureGaugeUsageRecordInline(admin.TabularInline):
+    model = PressureGaugeUsageRecord
+
+class PressureGaugeAdmin(admin.ModelAdmin):
+    search_fields = ('number', 'gauge__company', 'gauge__modus', 'gauge__type',  'usage_location__name')
+    list_display = ('__unicode__', 'gauge', 'usage_location')
+    list_filter = ('gauge__modus', 'gauge__company', 'usage_location__name')
+    save_on_top = True
+    ordering = ['number']
+    actions = ['excel_export']
+
+    inlines = [PressureGaugeUsageRecordInline]
+
+    def excel_export(self, request, queryset):
+        # Create the HttpResponse object with the appropriate CSV header.
+        response = HttpResponse(content_type='application/vnd.openxmlformats-officedocument.spreadsheetml.sheet')
+        response['Content-Disposition'] = 'attachment; filename="pressure_gauges.xslx"'
+
+        template_path = os.path.abspath(os.path.join(os.path.dirname(os.path.abspath(__file__)), 'pressure_gauges_template.xlsx'))
+
+        wb = load_workbook(template_path)
+        ws1 = wb.active
+
+        cis = queryset
+        row_offset = 2
+        i = 0
+        for ci in cis:
+            _ = ws1.cell(column=1, row=i+row_offset, value='{}'.format(ci.number))
+            _ = ws1.cell(column=2, row=i+row_offset, value='{}'.format(ci.gauge.company))
+            _ = ws1.cell(column=3, row=i+row_offset, value='{}'.format(ci.gauge.get_modus_display()))
+            _ = ws1.cell(column=4, row=i+row_offset, value='{}'.format(ci.usage_location))
+            i += 1
+
+        response.write(save_virtual_workbook(wb))
+        return response
+
+class PressureGaugeRecordAdmin(admin.ModelAdmin):
+    #search_fields = ('usage_location__name', 'user__name', 'date', 'gas_cylinder__cylinder_number', 'gas_cylinder__chemical__name')
+    #list_display = ('gas_cylinder', 'usage_location', 'user', 'date')
+    #list_filter = ('usage_location', 'user', 'date', 'gas_cylinder')
+
+    save_on_top = True
+
+
+admin.site.register(PressureGauge, PressureGaugeAdmin)
+admin.site.register(GaugeType)
+admin.site.register(PressureGaugeUsageRecord, PressureGaugeRecordAdmin)
---
+++ labinventory/models.py
@@ -0,0 +1,36 @@
+from django.db import models
+
+from cheminventory.models import Person, UsageLocation
+
+MODUS = (
+    ('PIR', 'Pirani'),
+    ('FULL', 'Full Range'),
+    ('COLD', 'Cold Cathode')
+)
+
+class GaugeType(models.Model):
+    company = models.CharField(max_length=30)
+    type = models.CharField(max_length=50)
+    modus = models.CharField(max_length=4, choices=MODUS)
+
+    def __unicode__(self):
+        return u'%s (%s)' % (self.get_modus_display(), self.company)
+
+class PressureGauge(models.Model):
+    number = models.CharField(max_length=4)
+    gauge = models.ForeignKey(GaugeType)
+    usage_location = models.ForeignKey(UsageLocation)
+
+    def __unicode__(self):
+        return u'%s' % (self.number)
+
+
+class PressureGaugeUsageRecord(models.Model):
+    gauge = models.ForeignKey(PressureGauge)
+    date = models.DateField()
+    user = models.ForeignKey(Person)
+    usage_location = models.ForeignKey(UsageLocation)
+    comment = models.CharField(max_length = 100, blank = True)
+
+    def __unicode__(self):
+        return u'Gauge %s used at %s on %s' % (self.gauge.number, self.usage_location, self.date)
