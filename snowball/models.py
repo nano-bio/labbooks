24e7495cf7f00c0fe0e394e25355ee013b1b2665
Felix Duensing <felix.duensing@uibk.ac.at>
2019-09-06 13:47:32 +0200

    snowball: shorten data file link

--- snowball/models.py
+++ snowball/models.py
@@ -1,33 +1,33 @@
 from django.db import models
-from django.core.validators import MaxValueValidator, MinValueValidator
-import time, datetime
-from django.core.exceptions import ValidationError
+import time
 from django.conf import settings
 
+
 class Operator(models.Model):
-    firstname = models.CharField(max_length = 50)
-    lastname = models.CharField(max_length = 50)
-    email = models.EmailField(max_length = 254)
+    firstname = models.CharField(max_length=50)
+    lastname = models.CharField(max_length=50)
+    email = models.EmailField(max_length=254)
 
     def __unicode__(self):
-        return u'%s %s' % (self.firstname, self.lastname)
+        return "{} {}.".format(self.firstname, self.lastname[:1])
+
 
 class Measurement(models.Model):
-    starttime = models.DateTimeField(auto_now = False, auto_now_add = False)
-    datafile = models.FileField(upload_to = 'snowball/%Y/%m/%d/%H/%M/%S/', max_length = 400)
+    starttime = models.DateTimeField(auto_now=False, auto_now_add=False)
+    datafile = models.FileField(upload_to='snowball/%Y/%m/%d/%H/%M/%S/', max_length=400)
     operator = models.ForeignKey('Operator')
-    he_temp = models.FloatField(verbose_name = 'He temp', default = float('9.5'))
-    he_pres = models.FloatField(verbose_name = 'He pressure', default = float('20'))
-    ee = models.FloatField(verbose_name = 'Electron Energy', default = float('70'))
-    ee_2 = models.FloatField(verbose_name = 'Electron Energy IS2', default = float('70'))
-    ec = models.FloatField(verbose_name = 'Electron Current', default = float('100'))
-    ec_2 = models.FloatField(verbose_name = 'Electron Current IS2', default = float('100'))
-    chan = models.FloatField(verbose_name = 'Channeltron Voltage', default = float('3500'))
-    text = models.TextField(max_length = 1500, verbose_name = 'Comment', blank = True)
-    short_description = models.CharField(max_length = 160, blank = True)
+    he_temp = models.FloatField(verbose_name='He temp', default=float('9.5'))
+    he_pres = models.FloatField(verbose_name='He pressure', default=float('20'))
+    ee = models.FloatField(verbose_name='Electron Energy', default=float('70'))
+    ee_2 = models.FloatField(verbose_name='Electron Energy IS2', default=float('70'))
+    ec = models.FloatField(verbose_name='Electron Current', default=float('100'))
+    ec_2 = models.FloatField(verbose_name='Electron Current IS2', default=float('100'))
+    chan = models.FloatField(verbose_name='Channeltron Voltage', default=float('3500'))
+    text = models.TextField(max_length=1500, verbose_name='Comment', blank=True)
+    short_description = models.CharField(max_length=160, blank=True)
 
     def __unicode__(self):
-        return u'%s, %s,' %(self.starttime, self.operator)
+        return u'%s, %s,' % (self.starttime, self.operator)
 
     def view_link(self):
         return "<a href='/vg/view/%s/'>View</a>" % (self.id)
@@ -43,12 +43,13 @@ class Measurement(models.Model):
         ordering = ['-starttime']
         get_latest_by = 'starttime'
 
+
 class JournalEntry(models.Model):
-    time = models.DateTimeField(auto_now = False, auto_now_add = True)
+    time = models.DateTimeField(auto_now=False, auto_now_add=True)
     operator = models.ForeignKey('Operator')
     comment = models.TextField()
-    attachment = models.FileField(upload_to = 'snowball/techjournal/', blank = True, default = '')
-    written_notes = models.ImageField(blank = True, upload_to = 'snowball/techjournal/notes/')
+    attachment = models.FileField(upload_to='snowball/techjournal/', blank=True, default='')
+    written_notes = models.ImageField(blank=True, upload_to='snowball/techjournal/notes/')
 
     def __unicode__(self):
         return u'%s, %s, %s: %s' % (self.id, self.time, self.operator, self.comment[:50])
@@ -60,19 +61,21 @@ class JournalEntry(models.Model):
         ordering = ['time']
         verbose_name_plural = "Journal Entries"
 
+
 class Turbopump(models.Model):
-    name = models.CharField(max_length = 100)
-    type = models.CharField(max_length = 100, blank = True)
-    purchase_date = models.DateField(auto_now_add = False, auto_now = False, blank = True, null = True)
-    service_date = models.DateField(auto_now_add = False, auto_now = False, blank = True, null = True)
-    
+    name = models.CharField(max_length=100)
+    type = models.CharField(max_length=100, blank=True)
+    purchase_date = models.DateField(auto_now_add=False, auto_now=False, blank=True, null=True)
+    service_date = models.DateField(auto_now_add=False, auto_now=False, blank=True, null=True)
+
     def __unicode__(self):
         return self.name
+
 
 class TurbopumpStatus(models.Model):
     pump = models.ForeignKey('Turbopump')
     current = models.FloatField()
-    date = models.DateField(auto_now_add = True, auto_now = False)
+    date = models.DateField(auto_now_add=True, auto_now=False)
 
     class Meta:
         verbose_name_plural = "Turbopump Status"
