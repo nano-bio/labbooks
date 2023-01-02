ef2902a23d0b1cbf712429b90ad32de00a6524d0
Graf-Zahl <felix@duensing.org>
2023-01-02 18:00:50 +0100

    moved django files to src/

--- gulpfile.js
+++ gulpfile.js
@@ -2,58 +2,58 @@ const {series, src, dest} = require('gulp');
 
 function bootstrap() {
   return src('node_modules/bootstrap/dist/**/*')
-    .pipe(dest('_vendor/bootstrap'))
+    .pipe(dest('src/_vendor/bootstrap'))
 }
 
 function bootstrapIcons() {
   return src('node_modules/bootstrap-icons/font/**/*')
-    .pipe(dest('_vendor/bootstrap-icons'))
+    .pipe(dest('src/_vendor/bootstrap-icons'))
 }
 
 function clusterize() {
   return src('node_modules/clusterize.js/clusterize*')
-    .pipe(dest('_vendor/clusterize'))
+    .pipe(dest('src/_vendor/clusterize'))
 }
 
 function dygraphs() {
   return src('node_modules/dygraphs/dist/**/*')
-    .pipe(dest('_vendor/dygraph'))
+    .pipe(dest('src/_vendor/dygraph'))
 }
 
 function flot() {
   return src('node_modules/flot/*.js')
-    .pipe(dest('_vendor/flot'))
+    .pipe(dest('src/_vendor/flot'))
 }
 
 function jquery() {
   return src('node_modules/jquery/dist/**/*')
-    .pipe(dest('_vendor/jquery'))
+    .pipe(dest('src/_vendor/jquery'))
 }
 
 function jqueryDeparam() {
   return src('node_modules/jquery-deparam/jquery-deparam.js')
-    .pipe(dest('_vendor/jquery.deparam'))
+    .pipe(dest('src/_vendor/jquery.deparam'))
 }
 
 function jqueryToast() {
   return src('node_modules/jquery-toast-plugin/dist/*')
-    .pipe(dest('_vendor/jquery.toast'))
+    .pipe(dest('src/_vendor/jquery.toast'))
 }
 
 
 function jsCookie() {
   return src('node_modules/js-cookie/dist/js.cookie.min.js')
-    .pipe(dest('_vendor/js.cookie'))
+    .pipe(dest('src/_vendor/js.cookie'))
 }
 
 function plotly() {
   return src('node_modules/plotly.js/dist/**/*')
-    .pipe(dest('_vendor/plotly'))
+    .pipe(dest('src/_vendor/plotly'))
 }
 
 function simpleLightBox() {
   return src('node_modules/simplelightbox/dist/**/*')
-    .pipe(dest('_vendor/simple-lightbox'))
+    .pipe(dest('src/_vendor/simple-lightbox'))
 }
 
 
